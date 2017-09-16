cea00b97e6150d12992e69253da5687c1eaef1c9
Trevor N <you@example.com>
2017-09-16 14:10:17 -0400

    setup firebase structure

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCV9KlVxmRAPFxWcUlsYAlfd__GgNua7Dk",
+  authDomain: "scratchpad-d2ebd.firebaseapp.com",
+  databaseURL: "https://scratchpad-d2ebd.firebaseio.com",
+  projectId: "scratchpad-d2ebd",
+  storageBucket: "",
+  messagingSenderId: "322610231154"
+};
+firebase.initializeApp(config);
